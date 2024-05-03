93bf92ca3028243746ee197fdcf556c413121014
David Roden <github-a8in@qsheltier.de>
2024-05-03 23:40:48 +0200

    ✅ Add creating commits with log message

--- svn-utils/src/test/kotlin/de/qsheltier/utils/svn/SimpleSVNTest.kt
+++ svn-utils/src/test/kotlin/de/qsheltier/utils/svn/SimpleSVNTest.kt
@@ -6,7 +6,9 @@ import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
+import org.tmatesoft.svn.core.SVNLogEntry
 import org.tmatesoft.svn.core.SVNURL
+import org.tmatesoft.svn.core.internal.io.fs.FSRepositoryFactory
 import org.tmatesoft.svn.core.io.SVNRepositoryFactory
 
 class SimpleSVNTest {
@@ -19,10 +21,22 @@ class SimpleSVNTest {
 	@Test
 	fun `SimpleSVN can create commit with username but without content`(@TempDir tempDir: File) {
 		val simpleSvn = SimpleSVN(createSvnRepository(tempDir))
-		val commitInfo = simpleSvn.createCommit("testuser") { _ ->
+		val commitInfo = simpleSvn.createCommit("testuser", "") { _ ->
 		}
 		assertThat(commitInfo.newRevision, equalTo(1))
 		assertThat(commitInfo.author, equalTo("testuser"))
+	}
+
+	@Test
+	fun `SimpleSVN can create commit with username and log message but without content`(@TempDir tempDir: File) {
+		val svnUrl = createSvnRepository(tempDir)
+		val simpleSvn = SimpleSVN(svnUrl)
+		simpleSvn.createCommit("testuser", "first commit") { _ ->
+		}
+		val svnRepository = FSRepositoryFactory.create(svnUrl)
+		val logEntries = mutableListOf<SVNLogEntry>()
+		svnRepository.log(arrayOf("/"), -1, -1, false, false, logEntries::add)
+		assertThat(logEntries.single().message, equalTo("first commit"))
 	}
 
 	private fun createSvnRepository(directory: File): SVNURL =
