59a9721675ba9071983700859cbde4cfc6c0aca5
David Roden <github-a8in@qsheltier.de>
2024-05-04 12:19:04 +0200

    ✅ Add basic version of repository scanner

---
+++ svn-utils/src/main/kotlin/de/qsheltier/utils/svn/RepositoryScanner.kt
@@ -0,0 +1,47 @@
+package de.qsheltier.utils.svn
+
+import org.tmatesoft.svn.core.SVNURL
+import org.tmatesoft.svn.core.io.SVNRepositoryFactory
+
+class RepositoryScanner(svnUrl: SVNURL) {
+
+	fun addBranch(name: String, vararg pathRanges: Pair<String, Pair<Long, Long>>) {
+		branchDefinitions.merge(name, pathRanges.toList()) { l1, l2 -> l1 + l2 }
+	}
+
+	fun identifyBranches(): Map<String, List<Long>> {
+		val branchRevisions = mutableMapOf<String, MutableList<Long>>()
+		val latestRevision = svnRepository.latestRevision
+
+		LongRange(1, latestRevision).forEach { revision ->
+			val branch = simpleSvn.getLogEntry("/", revision)!!
+				.changedPaths.keys
+				.mapNotNull { path -> findBranchByPathAndRevision(path, revision) }
+				.distinct()
+				.single()
+			branchRevisions.getOrPut(branch) { mutableListOf() }.add(revision)
+		}
+
+		return branchRevisions
+	}
+
+	private fun findBranchByPathAndRevision(path: String, revision: Long): String? =
+		branchDefinitions
+			.mapValues { (_, pathRanges) -> pathRanges.singleOrNull { (_, range) -> revision in range } }
+			.filter { it.value != null }
+			.filterValues { pathRanges -> path.startsWith(pathRanges!!.first) }
+			.keys.singleOrNull()
+
+	private val simpleSvn = SimpleSVN(svnUrl)
+	private val svnRepository = SVNRepositoryFactory.create(svnUrl)
+
+	private val branchDefinitions = mutableMapOf<String, Collection<Pair<String, Pair<Long, Long>>>>()
+
+}
+
+private operator fun Pair<Long, Long>.contains(value: Long) =
+	if (second != -1L) {
+		value in first..second
+	} else {
+		value >= first
+	}
--- svn-utils/src/test/kotlin/de/qsheltier/utils/svn/RepositoryScannerTest.kt
+++ svn-utils/src/test/kotlin/de/qsheltier/utils/svn/RepositoryScannerTest.kt
@@ -1,3 +1,50 @@
 package de.qsheltier.utils.svn
 
-class RepositoryScannerTest
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.contains
+import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.hasEntry
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import org.tmatesoft.svn.core.SVNURL
+import org.tmatesoft.svn.core.io.SVNRepositoryFactory
+
+class RepositoryScannerTest {
+
+	@Test
+	fun `repository scanner identifies branch correctly`() {
+		simpleSvn.createCommit("testuser", "create directory") { commit ->
+			commit.addDirectory("/project1")
+			commit.addFile("/project1/README", "project 1\n".byteInputStream())
+		}
+		simpleSvn.createCommit("testuser", "add second project") { commit ->
+			commit.addDirectory("/project2")
+			commit.addFile("/project2/README", "project 2\n".byteInputStream())
+		}
+		simpleSvn.createCommit("testuser", "add file to first project") { it.addFile("/project1/file1", "file 1\n".byteInputStream()) }
+		simpleSvn.createCommit("testuser", "add file to second project") { it.addFile("/project2/file2", "file 2\n".byteInputStream()) }
+		val repositoryScanner = RepositoryScanner(svnUrl)
+		repositoryScanner.addBranch("p1", "/project1" to (0L to -1))
+		repositoryScanner.addBranch("p2", "/project2" to (0L to -1))
+		val branches: Map<String, List<Long>> = repositoryScanner.identifyBranches()
+		assertThat(
+			branches, allOf(
+				hasEntry(equalTo("p1"), contains(1, 3)),
+				hasEntry(equalTo("p2"), contains(2, 4))
+			)
+		)
+	}
+
+	private fun createSvnRepository(directory: File): SVNURL =
+		SVNRepositoryFactory.createLocalRepository(Files.createTempDirectory(directory.toPath(), "svn-repo").toFile(), false, false)
+
+	@TempDir
+	private lateinit var temporaryDirectory: Path
+	private val svnUrl by lazy { createSvnRepository(Files.createTempDirectory(temporaryDirectory, "svn-repo").toFile()) }
+	private val simpleSvn by lazy { SimpleSVN(svnUrl) }
+
+}
