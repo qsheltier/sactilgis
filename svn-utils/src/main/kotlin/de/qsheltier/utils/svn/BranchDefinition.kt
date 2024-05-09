f61a795b270b048697e761b827adcf348d5cb2e1
David Roden <github-a8in@qsheltier.de>
2024-05-09 16:22:10 +0200

    🚸 Print branches without starting points at beginning

--- svn-utils/src/main/kotlin/de/qsheltier/utils/svn/BranchDefinition.kt
+++ svn-utils/src/main/kotlin/de/qsheltier/utils/svn/BranchDefinition.kt
@@ -4,8 +4,9 @@ import java.util.TreeMap
 
 class BranchDefinition(vararg revisionPaths: Pair<Long, String>) {
 
-	fun pathAt(revision: Long) = revisionPaths.floorEntry(revision)?.value
-
 	private val revisionPaths = TreeMap(mapOf(*revisionPaths))
+
+	fun pathAt(revision: Long) = revisionPaths.floorEntry(revision)?.value
+	val earliestRevision: Long = this.revisionPaths.firstEntry().key
 
 }
